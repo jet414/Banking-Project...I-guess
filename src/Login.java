49dd12abd41184231412d7283f8718e692649846
Eashan <eashan414@gmail.com>
2024-03-17 14:23:13 -0400

    Fully commented the Login.java file

--- src/Login.java
+++ src/Login.java
@@ -1,11 +1,11 @@
 import javax.swing.*;
-import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.util.HashMap;
 
 public class Login{
 
+    // initializing variables
     JFrame frame;
 
     HashMap<String, String> credentials;
@@ -22,42 +22,68 @@ public class Login{
 
     JOptionPane optionPane;
 
-    boolean isRobot;
+    boolean isNotRobot;
 
+    /*
+    All of my code is within the constructor. The Main.java class only opens the Login class, which begins the process.
+    */
     Login(){
 
+        // Assigning objects/variables
         frame = new JFrame();
         optionPane = new JOptionPane();
 
+        // Sample creds, will add a system to change this later.
         credentials = new HashMap<String, String>();
-        credentials.put("Halamcage", "414");
+        credentials.put("Eashan", "414");
 
+        // Label for username input field
         usernameField = new JLabel("Username");
         usernameField.setBounds(90,100,80,20);
 
+        // Label for password input field
         passwordField = new JLabel("Password");
         passwordField.setBounds(90,160,80,20);
 
+        //Enter username
         username = new JTextField();
         username.setBounds(215,100,200,20);
 
+        // Enter password
         password = new JPasswordField();
         password.setBounds(215,160,200,20);
 
+        // 'I am not a robot' check. I don't have a captcha test... YET...
         robot = new JRadioButton("I am not a robot");
         robot.setBounds(180,300,300,20);
-        robot.addActionListener(e -> {isRobot = true;});
+        robot.addActionListener(e -> {
+            isNotRobot = true;});
 
+        // Submit button. This button has most of the logic. Should I put it in a different method?
         submit = new JButton("Submit");
         submit.setBounds(179,330,70, 30);
         submit.addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
 
+                // Assigning what the user inputted to variables.
                 String usernameInput = username.getText();
                 String passwordInput = String.valueOf(password.getPassword());
 
-                if(credentials.containsKey(usernameInput) && isRobot){
+                /*
+                HOW THE MAIN VERIFICATION WORKS:
+
+                - If the Hashmap 'credentials' has the 'usernameInput' variable in it and the user is not a robot:
+                    - Checks if the 'passwordInput' matches the value for the key 'usernameInput' in HashMap 'credentials':
+                        - The 'Login' frame is disposed(meaning closed) and the 'Welcome' frame is launched.
+                    - If password is not correct:
+                        - JOptionPane shows a message dialog that the password is incorrect
+                - If HashMap 'credentials' does not have the 'usernameInput' as a key:
+                    - JOptionPane shows a message dialog saying that the entered username was incorrect
+                - Else:
+                    - JOptionPane shows a message dialog saying that the user is a robot.
+                */
+                if(credentials.containsKey(usernameInput) && isNotRobot){
                     if(passwordInput.equals(credentials.get(usernameInput))){
 
                         frame.dispose();
@@ -73,11 +99,10 @@ public class Login{
                 } else {
                     JOptionPane.showMessageDialog(null, "You are a robot");
                 }
-
-
             }
         });
 
+        // Reset button: When clicked, the button erases whatever is in the 'username' and 'password' fields.
         reset = new JButton("Reset");
         reset.setBounds(251,330,70,30);
         reset.addActionListener(new ActionListener() {
